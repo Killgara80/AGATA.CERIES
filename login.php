973a9b62a1ac611855da0f40b397e4925da4da36
Killgara80 <admi@hopvgskole.no>
2024-09-09 21:14:30 +0200

    Update login.php
    omfiguert coden til login siden

--- login.php
+++ login.php
@@ -2,16 +2,26 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"
+        content="IE-edge">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    
     <title>Login_Page</title>
 </head>
 <body>
-    <div class="Login">
-        <form>
-            <img src="" alt="" srcset="">
-        </form>
-
+    <div class="full-screen">
+     <div class="loggin-con">
+         <h1 class="loggin-tittle">hei</h1>
+         <form class="form">
+             <div class="input-group">
+                 <lable></lable>
+                 
+             </div>
+             </div>
+         </form>
+     </div>   
     </div>
 </body>
 </html>
